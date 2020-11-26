db94735b0d9909c95fdf67ac3fb3ef71988864b9
Garry Trinder <garry@trinder365.co.uk>
2020-11-26 01:32:28 +0000

    Remove setup files

--- .devcontainer/node.sh
+++
@@ -1,4 +0,0 @@
-# Enable NodeSource repository
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-# Install nodejs
-apt install nodejs -y
--- .devcontainer/prereqs.sh
+++
@@ -1,8 +0,0 @@
-# Update index and install packages
-apt-get update && apt-get install -y \
-  sudo \
-  zsh \
-  jq \
-  curl \
-  git \
-  wget 
